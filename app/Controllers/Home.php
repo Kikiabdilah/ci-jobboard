e87ea7bc13320f3bb7cba6ea3ca1eb178a813dcb
Muhamad Rizky Abdilah <Kiensa1234@gmail.com>
2025-05-24 19:45:25 +0700

    Display Job in Home Page

--- app/Controllers/Home.php
+++ app/Controllers/Home.php
@@ -1,11 +1,15 @@
 <?php
 
 namespace App\Controllers;
-
+use App\Models\Job\Job;
 class Home extends BaseController
 {
     public function index(): string
     {
-        return view('home');
+
+        $jobs = new Job();
+        $allJobs = $jobs->findAll();
+
+        return view('home', compact('allJobs'));
     }
 }
